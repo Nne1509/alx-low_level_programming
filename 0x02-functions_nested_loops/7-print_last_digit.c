553e3f648b3d97979f0b636768070842647d2aa1
Your Name <you@example.com>
2022-09-13 07:32:55 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,9 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 	{
-		return (last_digit * -1);
+		return (last_digit *= -1);
 	}
-	else
-	{
-		return (last_digit);
-	}
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
