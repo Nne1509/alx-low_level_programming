ecbc1b8e11a3bfae694e7a6bb47012ea52809733
Your Name <you@example.com>
2022-09-16 05:34:51 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints all numbers from 0 to 9
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}
