aa0c1e461d1668f3be273bd5856c3d2932ae6eaa
Your Name <you@example.com>
2022-09-19 06:31:57 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of string
+ * @str: string to print
+ * Return: second half of string
+ */
+
+void puts_half(char *str)
+{
+	int len, a, b;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (b = len / 2; str[b] != '\0'; b++)
+		{
+			_putchar(str[b]);
+		}
+	} else if (len % 2)
+	{
+		for (a = (len - 1) / 2; a < len - 1; a++)
+		{
+			_putchar(str[a + 1]);
+		}
+	}
+	_putchar('\n');
+}
