61875ecccb327262badaee2a1dad5eda39d21b49
Your Name <you@example.com>
2022-09-22 00:44:18 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,6 +4,7 @@
  * _strcmp - compares strings
  * @s1: first string
  * @s2: second string
+ *
  * Return: 0 if both strings are equal
  * less than 0 if s2 is greater than s1
  * more than 0 if s1 is greater than s2
