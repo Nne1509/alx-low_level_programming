6144f510bf3eea0d6d84bc3a01968cc4e46b742a
Your Name <you@example.com>
2022-10-11 13:27:04 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,12 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * struct dog - dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  *
- * Description: Longer description
+ * Description: Details of dog
  */
 
 struct dog
@@ -17,17 +17,18 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - typedef for struct dog
- */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - new dog
+ *
+ */
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
